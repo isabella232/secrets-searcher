dcfe35228d508e74f3cd35c1ef2945b4890162f6
Matt Alexander <matt.alexander@pantheon.io>
2020-04-13 16:44:48 -0500

    Fix generation deps again

--- pkg/database/enum/processor_type/processor_type_values.go
+++ pkg/database/enum/processor_type/processor_type_values.go
@@ -1,6 +1,6 @@
 package processor_type
 
-//go:generate sh -c "go-genums ProcessorType value string processor_type_values.go > processor_type.go"
+//go:generate sh -c "go run github.com/gdm85/go-genums ProcessorType value string processor_type_values.go > processor_type.go"
 
 const (
     valueURL     = "url"
--- pkg/database/enum/source_provider/source_provider_values.go
+++ pkg/database/enum/source_provider/source_provider_values.go
@@ -1,6 +1,6 @@
 package source_provider
 
-//go:generate sh -c "go-genums SourceProvider value string source_provider_values.go > source_provider.go"
+//go:generate sh -c "go run github.com/gdm85/go-genums SourceProvider value string source_provider_values.go > source_provider.go"
 
 const (
     valueLocal  = "local"
